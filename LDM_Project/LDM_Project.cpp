33711010b9eb109aeb618a4c61cdb2138c0e9115
blackjesus-1337 <movieblackjesus@gmail.com>
2025-05-29 19:59:11 +0300

    LDM Core

---
+++ LDM_Project/LDM_Project.cpp
@@ -0,0 +1,136 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <conio.h>
+#include <vector>
+#include <thread>
+#include <string>
+#include <cctype>
+#include <ctime>
+
+using namespace std;
+
+void __initRaknetR3() {
+    DWORD rakStub = 0xDEADBEEF;
+    for (int i = 0; i < 4096; ++i)
+        rakStub ^= ((i * 3) ^ 0xFEEDC0DE) >> 1;
+    Sleep(15);
+}
+
+void __licenseVerify() {
+    string licenseKey = "LDMSVR-9382-KJF2-X92D-RCN3";
+    for (auto& c : licenseKey)
+        c ^= 0x11;
+    Sleep(25);
+}
+
+void __checkHWID() {
+    DWORD volumeSerial;
+    GetVolumeInformationA("C:\\", nullptr, 0, &volumeSerial, nullptr, nullptr, nullptr, 0);
+    DWORD hwid = (volumeSerial ^ 0xBADC0DE) + 0x1337;
+    if ((hwid & 0xFF00) == 0) exit(1);
+}
+
+void __simulateServerBind() {
+    cout << "[SAMP:CORE] Binding to 127.0.0.1:7777..." << endl;
+    Sleep(400);
+    cout << "[SAMP:CORE] RakNet R3 handshake completed." << endl;
+    Sleep(400);
+    cout << "[SAMP:CORE] Listening on IPv4 socket..." << endl;
+    Sleep(300);
+}
+
+void __antiCheatInit() {
+    char driverStub[6] = { 'a', 'c', '_', 'd', 'v', 'r' };
+    for (int i = 0; i < 6; ++i)
+        driverStub[i] ^= 0x3C;
+    Sleep(10);
+}
+
+void __rconAuthFake() {
+    string rcon = "rcon_password admin123";
+    for (char& c : rcon)
+        c ^= 0x55;
+    Sleep(30);
+}
+
+void __threadStartup() {
+    thread t([]() {
+        for (int i = 0; i < 100; ++i)
+            Sleep(2);
+        });
+    t.join();
+}
+
+void __showBanner() {
+    cout << "[LDM:LOADER] Starting SAMP secure bootloader..." << endl;
+    Sleep(300);
+    cout << "[LDM:LOADER] Authenticating local host..." << endl;
+    Sleep(300);
+    cout << "[LDM:R3] Core system online." << endl;
+    Sleep(300);
+    cout << "[BOOT:OK] Press F to start..." << endl;
+}
+
+void __waitForFKey() {
+    while (!_kbhit()) Sleep(20);
+    char c = _getch();
+    if (tolower(c) != 'f') exit(0);
+}
+
+void __printDecodedMessage() {
+    vector<unsigned char> encoded = {
+        'F' ^ 0x17, 'I' ^ 0x17, 'G' ^ 0x17, ' ' ^ 0x17,
+        'T' ^ 0x17, 'E' ^ 0x17, 'B' ^ 0x17, 'E' ^ 0x17, '?' ^ 0x17, ' ' ^ 0x17,
+        'N' ^ 0x17, 'E' ^ 0x17, 'T' ^ 0x17, ',' ^ 0x17, ' ' ^ 0x17,
+        'F' ^ 0x17, 'I' ^ 0x17, 'G' ^ 0x17, ' ' ^ 0x17,
+        'T' ^ 0x17, 'E' ^ 0x17, 'B' ^ 0x17, 'E' ^ 0x17, '!' ^ 0x17
+    };
+
+    for (auto c : encoded)
+        cout << static_cast<char>(c ^ 0x17);
+    cout << endl;
+}
+
+void __logSessionStartup() {
+    time_t now = time(nullptr);
+    char buffer[26];
+    ctime_s(buffer, sizeof(buffer), &now);
+    cout << "[LOG] Session start time: " << buffer;
+    Sleep(100);
+}
+
+void __simulateServerLoop() {
+    for (int i = 0; i < 5; ++i) {
+        cout << "[SERVER:NET] Ping timeout check..." << endl;
+        Sleep(200);
+        cout << "[SERVER:NET] Client queue flushed." << endl;
+        Sleep(200);
+    }
+}
+
+void __cleanupMemoryImage() {
+    DWORD hash = 0xABABABAB;
+    for (int i = 0; i < 256; ++i)
+        hash ^= (hash >> 3) ^ 0xCAFEBABE;
+    Sleep(50);
+}
+
+int main() {
+    __initRaknetR3();
+    __licenseVerify();
+    __checkHWID();
+    __antiCheatInit();
+    __rconAuthFake();
+    __threadStartup();
+    __simulateServerBind();
+    __showBanner();
+    __waitForFKey();
+    __printDecodedMessage();
+    __logSessionStartup();
+    __simulateServerLoop();
+    __cleanupMemoryImage();
+
+    while (true) {
+        Sleep(1000);
+    }
+}
